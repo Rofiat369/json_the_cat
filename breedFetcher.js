56044bfd3310bf45ac3a7cdab83ec62ed848d96d
Rofiat Olusanya <rofiatolusanya12@gmail.com>
2021-12-02 21:57:34 +0000

    Refactored the cat code to look cleaner

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,22 +1,29 @@
 const request = require('request');
-// allows for user input in command line
-const breedName = process.argv.slice(2);
 
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+const fetchBreedDescription = (breedName, callback) => {
+  
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-request(url, (error, resp, body) => {
-  if (error) {
-    return console.log('Failed to request details: ', error);
+  request(url, (error, resp, body) => {
+    if (error) {
+      return callback(`Failed to request details: ${error}`, null);
+    }
+    
+    const data = JSON.parse(body);
+    
+    const breed = data[0];
+
+    if (breed) {
+     return callback(null,breed.description);
+    } else {
+      return callback(`Failed to find breed ${breedName}`, null);
+    }
   }
-  const data = JSON.parse(body);
-  const breed = data[0];
-  if (breed) {
-    console.log(breed.description);
-  } else {
-    console.log(`Failed to find breed ${breedName}`);
-  }
-}
-);
+  );
+};
+
+
+module.exports = { fetchBreedDescription };
 // turn string into object
 // locate first entry in the data array  and saves it in breed variable
 //print description is breed include in command line, else fails
